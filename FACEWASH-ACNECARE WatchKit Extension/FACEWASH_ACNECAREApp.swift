dfe0d3441dbd33d5aabb3dc0154122b572e64492
Nadia Ramadhani <103446529+twicetw@users.noreply.github.com>
2022-07-15 10:24:47 +0700

    Initial Commit

---
+++ FACEWASH-ACNECARE WatchKit Extension/FACEWASH_ACNECAREApp.swift
@@ -0,0 +1,21 @@
+//
+//  FACEWASH_ACNECAREApp.swift
+//  FACEWASH-ACNECARE WatchKit Extension
+//
+//  Created by Local Administrator on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FACEWASH_ACNECAREApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
